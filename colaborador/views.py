e97fd8324a62dcc8149e73387bccab905c672008
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2025-10-05 16:09:40 -0300

    refactor: generic forms

--- colaborador/views.py
+++ colaborador/views.py
@@ -5,7 +5,6 @@ from colaborador.forms import NewClientForm
 def home(request):
     return render(request, "colaborador/home.html")
 
-
 def new_client(request):
     form = NewClientForm()
-    return render(request, "colaborador/cadastrar_cliente.html", {"form": form})
+    return render(request, "colaborador/form.html", {"form": form})
