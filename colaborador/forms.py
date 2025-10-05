ffd8fa9f7f47a712661eb1cbe2b42ee108476589
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2025-10-04 21:38:04 -0300

    feat: initial test form

---
+++ colaborador/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Row, Column
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Row(
+                Column('name', css_class='col-md-6'),
+                Column('email', css_class='col-md-6'),
+            ),
+            'message',
+            Submit('submit', 'Send Message', css_class='btn btn-primary')
+        )
--- colaborador/views.py
+++ colaborador/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 def index(request):
-    return render(request, "colaborador/index.html")
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        # process form data
+        print(form.cleaned_data)
+    return render(request, "colaborador/index.html", {'form': form})
