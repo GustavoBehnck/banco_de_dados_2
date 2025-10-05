383608bd5ecc247f4fe39cc284a90508afe7a0de
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2025-10-04 22:33:17 -0300

    chore: menu and begin split

--- colaborador/views.py
+++ colaborador/views.py
@@ -4,14 +4,14 @@ from .forms import ContactForm
 
 def index(request):
     form = ContactForm(request.POST or None)
-    return render(request, "colaborador/index.html", {'form': form})
+    return render(request, "home/index.html", {'form': form})
 
 def about(request):
-    return render(request, "colaborador/about.html")
+    return render(request, "home/about.html")
 
 def contact(request):
     form = ContactForm(request.POST or None)
-    return render(request, "colaborador/contact.html", {'form': form})
+    return render(request, "home/contact.html", {'form': form})
 
 def cadastrar_modelo(request):
     form = ContactForm(request.POST or None)
