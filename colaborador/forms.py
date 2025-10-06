e436f5313e02eb2c5f8317665722b2c808ad5cf8
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2025-10-05 21:27:48 -0300

    fix contact form

--- colaborador/forms.py
+++ colaborador/forms.py
@@ -4,24 +4,6 @@ from crispy_forms.layout import Submit, Layout, Row, Column, HTML
 
 from .models import Client, Contract, Maintenance, Vehicle, VehicleModel
 
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method = 'post'
-        self.helper.layout = Layout(
-            Row(
-                Column('name', css_class='col-md-6'),
-                Column('email', css_class='col-md-6'),
-            ),
-            'message',
-            Submit('submit', 'Send Message', css_class='btn btn-primary')
-        )
-
 class NewClientForm(forms.ModelForm):
     class Meta:
         model = Client
