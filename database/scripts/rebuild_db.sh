fa879d033e809c5ecfcbefe32bb62d4bebeff5f0
MateusTakamatsu <mateus.takamatsu.dev@gmail.com>
2025-11-19 21:32:47 -0300

    Removing unnecessary comment

--- database/scripts/rebuild_db.sh
+++ database/scripts/rebuild_db.sh
@@ -1,18 +1,3 @@
-# Script to drop, create and populate grao_mestre database
-
-# Requirements to use this script
-# 1. All of the following script files must be in the same directory;
-#	a. create.sql
-#	b. populate.sql
-#	c. each_row_count.sql
-#	d. rebuild_db.sh
-# 2. You must have a .my.cnf file in your home directory (~/)
-#	a. Example can be found at example/example.my.cnf
-
-# Suggestion to make it easier
-# Create a function in your bashrc to run this script passing the first argument
-# 	a. Example can be found at example/bashrc_complement.bash
-
 base_dir_path="$1"
 action="$2"
 
