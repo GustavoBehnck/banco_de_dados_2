1a0e30f9cff8ca414dd47d6151947dc8241397e5
MateusTakamatsu <mateus.takamatsu.dev@gmail.com>
2025-11-26 20:38:32 -0300

    feat: Adding populate_maintenances.sql

---
+++ database/mysql_scripts/populate/python/generate_maintenances.py
@@ -0,0 +1,79 @@
+import random
+from datetime import datetime, timedelta
+
+# ----------------------------------------------------------------------------------------------------
+# Variáveis configuráveis para execução
+# ----------------------------------------------------------------------------------------------------
+nome_banco = 'grao_mestre_db'
+nome_tabela = 'maintenances'
+nome_do_arquivo_final = 'populate_' + nome_tabela
+extensao_do_arquivo = '.sql'
+
+pecas_tratores = ["Motor", "Transmissão", "Caixa de câmbio", "Sistema hidráulico", "Bomba hidráulica", "Braço hidráulico", "Pistões hidráulicos", "Sensores de proximidade", "GPS/GNSS", "Controlador eletrônico (ECU)", "Bateria", "Sistema de carregamento da bateria", "Atuadores de direção", "Sistema de freios", "Engate de implementos", "Esteiras/Rodas", "Lâminas de corte ou implementos de tração", "Cabos e chicotes elétricos", "Sistema de telemetria", "Radiadores / Sistema de arrefecimento"]
+jira_link_base = f"https://graomestre.atlassian.net/browse/TRACTOR_MAINT/"
+
+used_vehicle_ids = []
+
+def random_timestamp_string():
+    start = datetime(2020, 1, 1)
+    end = datetime(2025, 12, 31)
+    delta_total = int((end - start).total_seconds())
+    random_seconds = random.randint(0, delta_total)
+    random_dt = start + timedelta(seconds=random_seconds)
+    return random_dt.strftime("%Y%m%d%H%M%S")
+
+def random_timestamp(start_year=2022, end_year=2025, delta_days_max=365):
+    start = datetime(start_year, 1, 1, 0, 0, 0)
+    end = datetime(end_year, 12, 31, 23, 59, 59)
+    delta_total_seconds = int((end - start).total_seconds())
+    
+    created_dt = start + timedelta(seconds=random.randint(0, delta_total_seconds))
+    updated_dt = created_dt + timedelta(seconds=random.randint(0, int(timedelta(days=delta_days_max).total_seconds())))
+    
+    return created_dt.strftime('%Y-%m-%d %H:%M:%S'), updated_dt.strftime('%Y-%m-%d %H:%M:%S')
+
+
+# ----------------------------------------------------------------------------------------------------
+# Define um array de linhas para o script de inserção dos registros e insere as duas primeiras linhas
+# ----------------------------------------------------------------------------------------------------
+linhas_sql = [f'USE {nome_banco};', f'INSERT INTO {nome_tabela} (vehicle_id, date, reason, link_to_ticket, created_at, updated_at) VALUES']
+
+
+print("Iniciando a geração dos registros variados...")
+for i in range(1000):
+    # VEHICLE ID
+    while True:
+        vehicle_id = random.randint(1, 1000)
+        if vehicle_id not in used_vehicle_ids:
+            used_vehicle_ids.append(vehicle_id)
+            break
+
+    date, limbo = random_timestamp()
+
+    reason = "Dano em: " + random.choice(pecas_tratores)
+
+    link_to_ticket = jira_link_base + random_timestamp_string()
+
+    created_at, updated_at = random_timestamp()
+
+    nova_linha = f"('{vehicle_id}', '{date}', '{reason}', '{link_to_ticket}', '{created_at}', '{updated_at}'),"
+    linhas_sql.append(nova_linha)
+print("Registros gerados!")
+
+
+
+# ----------------------------------------------------------------------------------------------------
+# Transforma o array em um aqruivo de script SQL
+# ----------------------------------------------------------------------------------------------------
+
+# Substitui a vírgula no final do último registro por ponto e vírgula para encessar o SQL
+linhas_sql[-1] = linhas_sql[-1].rstrip(',') + ';'
+
+# Junta todas as linhas (mantendo a quebra de linhas entre elas) em um único texto
+script_sql = "\n".join(linhas_sql)
+
+# Cria um arquivo com o script de sql pronto para usar
+with open(f"{nome_do_arquivo_final}{extensao_do_arquivo}", "w") as f:
+    f.write(script_sql)
+
+print(f"Arquivo {nome_do_arquivo_final}{extensao_do_arquivo} criado com os INSERTs")
