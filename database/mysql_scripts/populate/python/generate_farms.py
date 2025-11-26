af02e760e79865c0df2d08d49869242bbf8fff00
MateusTakamatsu <mateus.takamatsu.dev@gmail.com>
2025-11-26 19:37:10 -0300

    feat: Adding populate_farms.sql

--- database/mysql_scripts/populate/python/generate_farms.py
+++ database/mysql_scripts/populate/python/generate_farms.py
@@ -5,20 +5,17 @@ from datetime import datetime, timedelta
 # Variáveis configuráveis para execução
 # ----------------------------------------------------------------------------------------------------
 nome_banco = 'grao_mestre_db'
-nome_tabela = 'farm_addresses'
-nome_do_arquivo_final = 'populate_farm_addresses'
+nome_tabela = 'farms'
+nome_do_arquivo_final = 'populate_farms'
 extensao_do_arquivo = '.sql'
 
-array1 = ["Rua", "Avenida", "Estrada", "Rodovia", "Alameda"]
-array2 = ["Ana", "Bruno", "Carla", "Daniel", "Eduardo", "Fernanda", "Gabriel", "Helena", "Igor", "João", "Karen", "Lucas", "Marina", "Nicolas", "Olivia", "Paulo", "Quésia", "Rafael", "Sara", "Tiago", "Ursula", "Valter", "Wesley", "Xavier", "Yara", "Zenaide", "Clara", "Miguel", "Sônia", "Rogério"]
-array3 = ["Silva", "Souza", "Costa", "Oliveira", "Pereira", "Rodrigues", "Almeida", "Nascimento", "Lima", "Araújo","Fernandes", "Gomes", "Martins", "Barros", "Cardoso", "Reis", "Cavalcante", "Dias", "Teixeira", "Moreira","Correia", "Ramos", "Melo", "Freitas", "Borges", "Campos", "Farias", "Sales", "Monteiro", "Rocha"]
+arraynomes = ["Fazenda", "Sítio", "Chácara", "Estância", "Haras"]
+arraysobrenomes1 = ["Do Milho", "Da Soja", "Do Trigo", "Do Café", "Do Gado", "Do Leite", "Da Pecuária", "Dos Cavalos", "Do Pasto", "Do Curral", "Do Algodão", "Da Cana", "Do Arroz", "Da Lavoura", "Do Cerrado", "Da Terra Vermelha", "Do Capim", "Do Eucalipto", "Do Pomar", "Do Laticínio", "Da Semente", "Do Transporte", "Da Ração", "Do Celeiro", "Da Colheita", "Do Trator", "Do Arado", "Da Plantação", "Do Campo", "Do Estábulo", "Da Pecuária Leiteira", "Dos Suínos", "Das Ovelhas", "Da Horta", "Do Viveiro", "Do Lago", "Da Granja", "Do Engenho", "Do Silo", "Do Galpão"]
+arraysobrenomes2 = ["Alta", "Verde", "Nova", "Antiga", "Bonita", "Produtiva", "Grande", "Pequena", "Do Vale", "Da Serra", "Do Sol", "Da Lua", "Da Mata", "Da Colina", "Do Horizonte", "Do Rio", "Da Lagoa", "Da Montanha", "Do Morro", "Do Planalto", "Do Luar", "Da Neblina", "Do Vento", "Da Chuva", "Da Esperança", "Da União", "Da Vitória", "Da Harmonia", "Da Paz", "Do Amanhecer", "Do Entardecer", "Do Cerrado", "Da Aurora", "Da Várzea", "Da Pradaria", "Do Vale Dourado", "Da Folha Verde", "Do Caminho", "Do Entorno"]
 
-array4 = ["Parque", "Vila", "Vale", "Morro", "Encosta", "Alto", "Monte", "Serra", "Bosque","Floresta", "Planalto", "Planície", "Colinas", "Campos", "Gramado", "Veredas"]
-array5 = ["Cedro", "Carvalho", "Jacarandá", "Ipê", "Sândalo", "Cristal", "Quartzo", "Granito", "Riacho", "Lago", "Fonte", "Pedra", "Amanhecer", "Horizonte", "Caminho", "Neblina", "Orvalho", "Raiz", "Folhagem", "Ventania", "Trilha", "Aroeira", "Limoeiro", "Pinheiro","Jatobá", "Bambu", "Cerrado", "Trigal", "Palmeira", "Cedrilho"]
-array6 = ["Dourado", "Verde", "Claro", "Escuro", "Sereno", "Silvestre", "Antigo", "Novo", "Profundo", "Aberto", "Brilhante", "Frondoso", "Sublime", "Calmo", "Vivo", "Resistente","Forte", "Suave", "Puro", "Radiante", "Natural", "Harmonioso", "Longo", "Distante","Alto", "Baixo", "Rico", "Raro", "Sagrado", "Pleno"]
 
-cidades_brasil = [["São Paulo", "São Paulo"], ["Rio de Janeiro", "Rio de Janeiro"], ["Belo Horizonte", "Minas Gerais"],["Brasília", "Distrito Federal"], ["Salvador", "Bahia"], ["Fortaleza", "Ceará"], ["Recife", "Pernambuco"],["Curitiba", "Paraná"], ["Porto Alegre", "Rio Grande do Sul"], ["Manaus", "Amazonas"],["Belém", "Pará"], ["Goiânia", "Goiás"], ["Campinas", "São Paulo"], ["Niterói", "Rio de Janeiro"], ["Florianópolis", "Santa Catarina"],["Santos", "São Paulo"], ["Maceió", "Alagoas"], ["Natal", "Rio Grande do Norte"], ["Vitória", "Espírito Santo"], ["João Pessoa", "Paraíba"],["Campo Grande", "Mato Grosso do Sul"], ["Cuiabá", "Mato Grosso"], ["Teresina", "Piauí"], ["Aracaju", "Sergipe"],["Uberlândia", "Minas Gerais"], ["Ribeirão Preto", "São Paulo"], ["Sorocaba", "São Paulo"], ["Londrina", "Paraná"], ["Maringá", "Paraná"], ["Caxias do Sul", "Rio Grande do Sul"],["Pelotas", "Rio Grande do Sul"], ["Joinville", "Santa Catarina"], ["Blumenau", "Santa Catarina"], ["Chapecó", "Santa Catarina"],["Foz do Iguaçu", "Paraná"], ["Macapá", "Amapá"], ["Boa Vista", "Roraima"], ["Porto Velho", "Rondônia"], ["Palmas", "Tocantins"], ["Imperatriz", "Maranhão"]]
-cidades_usa = [["New York", "New York"], ["Los Angeles", "California"], ["Chicago", "Illinois"],["Houston", "Texas"], ["Phoenix", "Arizona"], ["Philadelphia", "Pennsylvania"],["San Antonio", "Texas"], ["San Diego", "California"], ["Dallas", "Texas"], ["San Jose", "California"]]
+used_client_ids = []
+used_address_ids = []
 
 def random_timestamp(start_year=2022, end_year=2025, delta_days_max=365):
     start = datetime(start_year, 1, 1, 0, 0, 0)
@@ -34,28 +31,33 @@ def random_timestamp(start_year=2022, end_year=2025, delta_days_max=365):
 # ----------------------------------------------------------------------------------------------------
 # Define um array de linhas para o script de inserção dos registros e insere as duas primeiras linhas
 # ----------------------------------------------------------------------------------------------------
-linhas_sql = [f'USE {nome_banco};', f'INSERT INTO {nome_tabela} (cep, street, number, complement, neighborhood, city, country_subdivision, country, created_at, updated_at) VALUES']
+linhas_sql = [f'USE {nome_banco};', f'INSERT INTO {nome_tabela} (name, client_id, address_id, area, status, created_at, updated_at) VALUES']
 
 
 print("Iniciando a geração dos registros variados...")
 for i in range(1000):
-    cep = f"{''.join([str(random.randint(0, 9)) for _ in range(5)])}-{str(i).zfill(3)}"
-    street = f"{random.choice(array1)} {random.choice(array2)} {random.choice(array3)}"
-    number = "S/ Nº" if random.random() < 0.01 else str(random.randint(10, 3500))
-    complement = random.choice(["'Chácara'", "'Sítio'", "'Fazenda'", "NULL"])
-    neighborhood = f"{random.choice(array4)} {random.choice(array5)} {random.choice(array6)}"
+    name = f"{random.choice(arraynomes)} {random.choice(arraysobrenomes1)} {random.choice(arraysobrenomes2)}"
 
-    # Escolhe se será Brasil ou EUA (80% Brasil, 20% EUA)
-    if random.random() < 0.8:
-        city, country_subdivision = random.choice(cidades_brasil)
-        country = "Brazil"
-    else:
-        city, country_subdivision = random.choice(cidades_usa)
-        country = "USA"
+    # CLIENT ID
+    while True:
+        client_id = random.randint(1, 1008)
+        if client_id not in used_client_ids:
+            used_client_ids.append(client_id)
+            break
+
+    # ADDRESS ID
+    while True:
+        address_id = random.randint(1, 1000)
+        if address_id not in used_address_ids:
+            used_address_ids.append(address_id)
+            break
+
+    area = round(random.uniform(0.5, 5000), 2)
+    status = f"{random.choice(['active', 'inactive', 'suspended'])}"
 
     created_at, updated_at = random_timestamp()
 
-    nova_linha = f"('{cep}', '{street}', '{number}', {complement}, '{neighborhood}', '{city}', '{country_subdivision}', '{country}', '{created_at}', '{updated_at}'),"
+    nova_linha = f"('{name}', '{client_id}', '{address_id}', {area}, '{status}', '{created_at}', '{updated_at}'),"
     linhas_sql.append(nova_linha)
 print("Registros gerados!")
 
