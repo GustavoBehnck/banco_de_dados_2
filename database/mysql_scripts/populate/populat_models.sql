250f97f5868441cee516f1977a4e1f370c2a8859
GustavoBehnck <gustavobehnckcardoso@gmail.com>
2025-11-26 20:22:06 -0300

    feat: new populate models

---
+++ database/mysql_scripts/populate/populat_models.sql
@@ -0,0 +1,8 @@
+-- Insert 5 agriculture truck models
+INSERT INTO models (type, batery_capacity, fabrication_year, charging_time)
+VALUES 
+    ('planting', 150.5, 2023, 4.5),
+    ('spraying', 120.0, 2024, 3.0),
+    ('harvesting', 300.0, 2022, 6.5),
+    ('planting', 180.2, 2024, 5.0),
+    ('spraying', 135.5, 2023, 3.5);
